9cbc670811203909883d01fd16ba3bc044d46efb
matias9486 <matias9486mdp@gmail.com>
2025-01-28 23:20:04 -0300

    feat: implementación de función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//Definición de variables
+let amigos = [];
+
+
+function agregarAmigo(){
+    //Capturar el valor del campo de entrada
+    let nombreAmigo = document.getElementById('amigo').value;
+    if(validarEntrada(nombreAmigo)){
+        // agregar amigo a lista
+        actualizarAmigos(nombreAmigo);        
+        // limpiar entrada de nombre
+        limpiarCampo('amigo');
+        // mostrar lista
+        alert(amigos);
+    }    
+    else
+        alert(`Por favor, inserte un nombre.`);
+}
+
+//validación para asegurarse de que el campo no esté vacío
+function validarEntrada(texto){
+    // Elimina los espacios al principio y al final
+    const textoLimpio = texto.trim();
+
+    // Verifica si el texto no está vacío
+    return textoLimpio.length !== 0? true : false; 
+}
+
+//limpia el campo de entrada
+function limpiarCampo(campo){
+    document.getElementById(campo).value = '';
+}
+
+//Actualiza el array de amigos
+function actualizarAmigos(nombre){
+    amigos.push(nombre);
+}
+
